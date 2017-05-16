f41c5047a707c1e19651fa0e5aca4ae5e74e0d4a
JimWilcox3 <jim.wilcox@aimovers.com>
2017-05-16 10:25:56 -0400

    Initial Version

--- VisualStylesTest/Form1.Designer.vb
+++ VisualStylesTest/Form1.Designer.vb
@@ -22,9 +22,37 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(56, 32)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(114, 20)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(56, 64)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(347, 252)
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents MonthCalendar1 As MonthCalendar
 End Class
---
+++ VisualStylesTest/Main.vb
@@ -0,0 +1,10 @@
+﻿Public Class Main
+    Public Shared Sub Main()
+
+        Application.EnableVisualStyles()
+
+        Application.Run(Form1)
+
+    End Sub
+
+End Class
